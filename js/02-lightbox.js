7db409a5a9bee1c916fb3c6a2022819ff5c7bbc9
DenisSkv <19phantom95@gmail.com>
2023-02-17 01:21:33 +0200

    finished

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const items = [];
+
+galleryItems.forEach((element) => {
+
+const galleryLink =
+document.createElement("a");
+galleryLink.classList.add("gellery__link");
+galleryLink.href = element.original;
+
+const galleryImage = document.createElement("img");
+galleryImage.classList.add("gallery__img");
+galleryImage.src = element.preview;
+galleryImage.setAttribute("title", element.description);
+galleryImage.alt = element.description;
+
+galleryLink.append(galleryImage);
+items.push(galleryLink);
+    
+
+});
+
+gallery.append(...items);
+
+new SimpleLightbox(".gallery a", { captionDelay: 250, });
+
